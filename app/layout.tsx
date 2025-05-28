0e040c66748ae6cfc8c62978a2930334ff2a04a0
cullen kaberere <kabesh86@gmail.com>
2025-05-28 22:11:59 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Wrap & Drive',
   description: 'Created with v0',
   generator: 'v0.dev',
+  icons: {
+      icon: "/Fav8.png", 
+    }
 }
 
 export default function RootLayout({
